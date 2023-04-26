16e144bd4b1a494467f07b25e17e6c360159ad3c
Jose <joseluis0528@gmail.com>
2023-04-25 20:50:13 -0500

    added first 2 functions

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(array) {
+    return array.map(employee => createEmployeeRecord(employee))
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
